ae81aa92e08e98926c103add4d90de008343c52b
Taniya  Baral <108205147+7taniya@users.noreply.github.com>
2023-06-07 00:32:27 +0530

    Add files via upload

---
+++ JavaQ10.java
@@ -0,0 +1,9 @@
+public class JavaQ10 {
+    // Pre-Increment operator 
+    public static void main(String args[]){   
+    int a=10;
+    int b=++a;
+    System.out.println(a);
+    System.out.println(b);
+}
+}
