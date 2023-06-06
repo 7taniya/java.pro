2e4a69834ee91555777f4169012859c11bf1648d
Taniya  Baral <108205147+7taniya@users.noreply.github.com>
2023-06-07 01:30:22 +0530

    Add files via upload

---
+++ JavaQ12.java
@@ -0,0 +1,11 @@
+public class JavaQ12 {
+    // Pre-Drecrement Operator
+    public static void main(String args[]){
+        int a=10;
+        int b=--a;
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+    
+}
---
+++ JavaQ14.java
@@ -0,0 +1,14 @@
+public class JavaQ14 {
+// Largest of the two numbers
+    public static void main(String args[]){
+    int A=5;
+    int B=10;
+    if(A>=B){
+        System.out.println("A is the largest");
+    }
+    else{
+        System.out.println("B is the largest ");
+    }
+    }
+    
+}
