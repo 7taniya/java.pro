3ee1a73a0d3a4b306444ec1e9090e4b8450ca44b
Taniya  Baral <108205147+7taniya@users.noreply.github.com>
2023-06-06 20:34:47 +0530

    Add files via upload

---
+++ JavaQ6.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class JavaCN {
+    //char to integer type
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        char ch= sc.next().charAt(0);//input for character
+        int number=ch;
+        System.out.println(number);
+    }
+}
