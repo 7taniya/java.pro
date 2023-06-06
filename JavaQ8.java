b86d6bc358c173572e21f78aca3a84954b51189c
Taniya  Baral <108205147+7taniya@users.noreply.github.com>
2023-06-06 20:47:34 +0530

    Add files via upload

---
+++ JavaQ8.java
@@ -0,0 +1,9 @@
+public class JavaQ8 {
+    // to convert integer into byte
+    public static void main(String args[]){
+        byte b=5;
+        byte a=(byte)(b*2);
+        System.out.println(a);
+
+    }
+}
