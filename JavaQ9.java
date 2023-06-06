a62066942eafe80b09f5455a09d477c5b9e1c3cb
Taniya  Baral <108205147+7taniya@users.noreply.github.com>
2023-06-07 00:07:42 +0530

    Add files via upload

---
+++ JavaQ9.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class JavaDou {
+    //Typecasting to double type
+    public static void main(String args[]){
+        int a=10;
+        float b=34.5f;
+        long c=59078l;
+        double d=298.5;
+        double ans=a+b+c+d;
+        System.out.println(ans);
+    }
+}
