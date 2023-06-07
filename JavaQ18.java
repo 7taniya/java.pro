4edeba0ae18cf5661679cba5aa6ce1069dfbcfba
Taniya  Baral <108205147+7taniya@users.noreply.github.com>
2023-06-07 15:24:00 +0530

    Add files via upload

---
+++ JavaQ18.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class JavaQ18 {
+    //Largest of Three numbers
+     public static void main(String args[]){
+        int A=5,B=6,C=4;
+        if(A>=B && A>=C){
+          System.out.println("A is the largest");}
+        else if(B>=C){
+        System.out.println("B is the largest");
+          }
+          else{
+            System.out.println("C is the largest");
+          }
+        
+     }
+}
